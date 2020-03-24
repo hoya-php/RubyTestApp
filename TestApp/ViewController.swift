bc00788d8d548266c78739663508a551f93ebf4c
hoya <49055272+hoya-php@users.noreply.github.com>
2020-03-24 06:38:18 -0700

    ”File Add”

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -35,6 +35,7 @@ class ViewController: UIViewController {
                     
                 self.outputRuby.text = inputText
                 self.outputCharacter.text = responseData.converted
+                    
             })
         }
     }
